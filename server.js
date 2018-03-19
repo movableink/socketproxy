25490ef7abd7a6ca49da6bf9fdc865b1b34c48ca
Michael Nutt <michael@nuttnet.net>
2018-03-19 00:28:49 -0400

    more robust server binary

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 const express = require('express');
-const http = require('http');
 const url = require('url');
+const http = require('http');
+const https = require('https');
 const WebSocket = require('ws');
 const uuid = require('uuid/v4');
+const EventEmitter = require('events');
+const ip = require('ip');
 
 function randomId() {
   return 'i-' + uuid();
 }
 
+function connectionIdFromHost(hostname) {
+  return hostname && hostname.split('.')[0];
+}
+
 // Things to serialize from the http IncomingMessage object
 const reqKeys = ['headers', 'rawHeaders', 'readable', 'domain', 'trailers', 'rawTrailers', 'url', 'method', 'upgrade', 'baseUrl', 'originalUrl', 'params', 'query'];
 
-class SocketProxyServer {
+class SocketProxyServer extends EventEmitter {
   constructor(opts={}) {
-    this.connections = new Map();
+    super();
 
+    this.opts = opts;
+    this.secure = !!(opts.sslCert || opts.sslKey);
+    this.proto = this.secure ? 'https' : 'http';
+    this.wsProto = this.secure ? 'ws' : 'wss';
+
+    this.connections = new Map();
     this.app = this.buildApp();
-    this.server = http.createServer(this.app);
+
+    if(this.secure) {
+      this.server = https.createServer({
+        key: opts.sslKey,
+        cert: opts.sslCert
+      }, this.app);
+    } else {
+      this.server = http.createServer(this.app);
+    }
+
     this.wss = this.buildWebSockets(this.server);
   }
 
@@ -32,7 +54,22 @@ class SocketProxyServer {
 
   handleRequest(req, res) {
     const hostname = req.headers.host;
-    const connectionId = hostname.split('.')[0];
+    if (!hostname) {
+      res.statusCode = 400;
+      res.end('Host header is required');
+      return;
+    }
+
+    const remoteIp = req.socket.remoteAddress;
+    const allowed = this.opts.allowHttpSubnet;
+
+    if (allowed && !ip.cidrSubnet(allowed).contains(remoteIp)) {
+      res.statusCode = 403;
+      res.end('Forbidden');
+      return;
+    }
+
+    const connectionId = connectionIdFromHost(hostname);
 
     const requestId = randomId();
 
@@ -73,9 +110,14 @@ class SocketProxyServer {
     }
   }
 
-  listen(port, callback) {
+  listen(port, bind, callback) {
+    if(typeof(bind) === 'function') {
+      callback = bind;
+      bind = '0.0.0.0';
+    }
+
     return new Promise((resolve, reject) => {
-      return this.server.listen(port, (err) => {
+      return this.server.listen(port, bind, (err) => {
         if(callback) { callback(err); }
 
         if(err) {
@@ -98,7 +140,6 @@ class SocketProxyServer {
   buildWebSockets(server) {
     const wss = new WebSocket.Server({ server });
     wss.on('connection', (ws, req) => {
-
       const location = url.parse(req.url, true);
       const host = req.headers.host;
 
@@ -116,6 +157,7 @@ class SocketProxyServer {
       });
 
       ws.send(isConnected);
+      this.emit('connection', req, connectionId);
     });
 
     return wss;
