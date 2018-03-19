d88842be2a33e4a50f88a18c95943b62267c0146
Michael Nutt <michael@nuttnet.net>
2018-03-19 16:39:07 -0400

    split out handleMessage, better connect logic that reconnects if necessary

--- index.js
+++ index.js
@@ -13,43 +13,78 @@ class SocketProxy {
     this.app = opts.app;
   }
 
-  connect() {
-    this.ws = new WebSocket(this.url);
+  handleMessage(data) {
+    const msg = JSON.parse(data);
+    if(msg.type === 'request') {
+      const socket = new FakeSocket(this.ws, msg.id);
+      const req = new http.IncomingMessage(socket);
+      Object.assign(req, msg.request);
 
-    return new Promise((resolve, reject) => {
-      this.ws.on('error', (err) => {
-        reject(err);
+      const res = new ServerResponse(req);
+      res.assignSocket(socket);
+
+      res.on('finish', () => {
+        this.ws.send(JSON.stringify({
+          type: 'finish',
+          id: msg.id
+        }));
       });
 
-      this.ws.on('message', (data) => {
-        data = JSON.parse(data);
-        if(data.type === 'request') {
-          const socket = new FakeSocket(this.ws, data.id);
-          const req = new http.IncomingMessage(socket);
-          Object.assign(req, data.request);
+      this.app.handle(req, res);
+    } else if(msg.type === 'status') {
+      this.lastStatus = msg;
+    } else {
+      console.log('unknown data', msg);
+    }
+  }
 
-          const res = new ServerResponse(req);
-          res.assignSocket(socket);
-
-          res.on('finish', () => {
-            this.ws.send(JSON.stringify({
-              type: 'finish',
-              id: data.id
-            }));
-          });
-
-          this.app.handle(req, res);
-        } else if(data.type === 'status') {
-          resolve(data);
+  ping() {
+    return new Promise((resolve, reject) => {
+      this.ws.send(JSON.stringify({ type: 'ping' }), (err) => {
+        if(err) {
+          reject(err);
         } else {
-          console.log('unknown data', data);
+          resolve();
         }
       });
     });
   }
 
+  connect() {
+    if(this.ws) {
+      // we have an existing socket session, see if it is active
+      return this.ping().then(() => {
+        return this.lastStatus;
+      }).catch((e) => {
+        // ping failed, reset socket and try reconnecting
+        this.ws = null;
+        return this.connect();
+      });
+    }
+
+    return new Promise((resolve, reject) => {
+      this.ws = new WebSocket(this.url);
+
+      this.ws.on('error', (err) => {
+        this.ws = null; // try reconnecting next time
+        reject(err);
+      });
+
+      // First message on connection will be a status message
+      this.ws.once('message', (data) => {
+        this.lastStatus = JSON.parse(data);
+        resolve(this.lastStatus);
+      });
+
+      this.ws.on('message', (data) => {
+        this.handleMessage(data);
+      });
+    });
+  }
+
   close() {
-    return this.ws.close();
+    this.ws.close();
+    this.ws = null;
   }
 }
 
